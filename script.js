da2becf4620e315f0c104250590307ad7106a8b1
YURI MEIRA MARTINS <yuri.mmartins2@senacsp.edu.br>
2023-05-31 21:13:30 -0300

    teste java

---
+++ script.js
@@ -0,0 +1,8 @@
+// constante não muda o valor.
+const $html = document.querySelector('html');
+const $checkbox = document.querySelector('#switch');
+
+$checkbox.addEventListener('change', function(){
+    $html.classList.toggle('dark-mode');
+
+})
